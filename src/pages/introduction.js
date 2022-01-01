368025cedb44625a7e1da6d215dd5d4283eb342c
4l3x4nd3r4u1 <alexander.ag150@gmail.com>
2021-12-31 22:40:53 -0600

    introduccion

--- src/pages/introduction.js
+++ src/pages/introduction.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import { HStack, VStack, Heading } from '@chakra-ui/react';
+import { HStack, VStack, Heading, Text } from '@chakra-ui/react';
 
 const Introduction = () => {
   return (
     <HStack h='full'>
-      <VStack w='full'><Heading>Introduction</Heading></VStack>
+      <VStack w='full'>
+      <Heading>Introduction</Heading>
+        <Text>introduction</Text>
+      </VStack>
     </HStack>
   );
 }
